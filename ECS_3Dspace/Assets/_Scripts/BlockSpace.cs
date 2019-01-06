3fff7144efd60071484adaae447aafb6c73f878b
Kyle MacMillan <macattackftw@gmail.com>
2019-01-06 09:38:39 -0700

    Space partitioning in work
    Since ECS doesn't have colliders it is necessary to efficiently know distances to objects. It was decided that space will be cut into octants. The entire space will be subdivided into 729 blocks (9x9x9), with each octant being 5x5x5 with an overlap on 3 planes. Each octant can be recursively built as a "new space" to reduce the number of entities in that space.
    
    It is "expensive" to run distance calculations against each entities for all other entities. This segmentation of space allows for smaller groupings of entities so shortest distance is faster to calculate. This means the space will have to be built each frame but segmentation of space is order O(N), and distance calculations will be at most O(N^2) on each octant.
    
    Octant recursive calls will be determined by an entity threshold count. If an octant contains more than X entities it will be subdivided into octants. Since subdivision is O(N) this isn't prohibitive.

---
+++ ECS_3Dspace/Assets/_Scripts/BlockSpace.cs
@@ -0,0 +1,46 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Space
+{
+    /// <summary>
+    /// Recursive class that manages collisions and nearest ship
+    /// </summary>
+    public class BlockSpace
+    {
+        private const int SpaceSize = 729;          // 9x9x9 blocks
+        private const int BlocksPerChunk = 125;     // 5x5x5 blocks
+        private const int N = 9;                    // Blocks per side
+        private const int Chunks = 8;               // There are 8: 5x5x5 chunks
+        private Block[] blocks;
+
+        public BlockSpace(float3 Lower, float3 Upper, NativeArray<Entity> Entities)
+        {
+            float3 increment = new float3((Upper - Lower) / N);
+            for (int i = 0; i < SpaceSize; ++i)
+            {
+                blocks[i] = new Block(Lower + (i * increment), increment);
+            }
+        }
+    }
+
+    public class Chunk
+    {
+
+    }
+
+    public class Block
+    {
+        private float3 lower;
+        private float3 upper;
+        public NativeArray<Entity> entities;
+
+        public Block(float3 Lower, float3 Increment)
+        {
+            lower = Lower;
+            upper = Lower + Increment;
+            entities = new NativeArray<Entity>();
+        }
+    }
+}
